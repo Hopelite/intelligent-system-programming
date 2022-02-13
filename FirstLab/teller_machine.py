20de1692b330d6e7839f1c0612b01306b61fc1e1
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-02-13 17:50:04 +0300

    feat: add banknote model

---
+++ FirstLab/teller_machine.py
@@ -0,0 +1,15 @@
+import datetime
+from decimal import Decimal
+
+class Banknote:
+    def __init__(self, value: Decimal) -> None:
+        if value <= 0:
+            raise InvalidBancknoteValueException("Unable to create a banknote with value that is negative or equals zero.")
+        self.__value = value
+
+    @property
+    def value(self) -> Decimal:
+        return self.__value
+
+class InvalidBancknoteValueException(Exception):
+    pass
