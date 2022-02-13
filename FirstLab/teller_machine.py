a4084e3b4a861916c8233f972070e7c3156d22ee
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-02-13 20:55:01 +0300

    feat: implement banknote storage; add docs

--- FirstLab/teller_machine.py
+++ FirstLab/teller_machine.py
@@ -1,6 +1,6 @@
-import datetime
 from decimal import Decimal
 
+# Represents the banknote model
 class Banknote:
     def __init__(self, value: Decimal) -> None:
         if value <= 0:
@@ -11,5 +11,69 @@ class Banknote:
     def value(self) -> Decimal:
         return self.__value
 
+    def __lt__(self, other):
+        return self.value < other.value
+        
+    def __gt__(self, other):
+        return self.value > other.value
+        
+    def __eq__(self, other):
+        return self.value == other.value
+
+# Exception raised when trying to pass invalid banknote value
 class InvalidBanknoteValueException(Exception):
+    pass
+
+# Contains methods for banknote storage
+class IBanknoteStorage:
+    def __subclasscheck__(self, __subclass: type) -> bool:
+        return (hasattr(__subclass, "get_cash_available") and callable(__subclass.get_cash_available) \
+        and hasattr(__subclass, "withdraw_banknotes") and callable(__subclass.withdraw_banknotes) \
+        and hasattr(__subclass, "deposit_banknotes") and callable(__subclass.deposit_banknotes))
+
+# Implements methods for operationing with banknotes storage
+class BanknoteStorage(IBanknoteStorage):
+    def __init__(self, banknotes: list[Banknote] = []) -> None:
+        self.__banknotes = banknotes
+        
+    # Calculates the sum of all banknotes values in storage 
+    def get_cash_available(self) -> Decimal:
+        amount = Decimal()
+        for banknote in self.__banknotes:
+            amount += banknote.value
+
+        return amount
+
+    # Withdraws banknotes from storage with an amount equal specified or the nearest available to it, if storage doesn't have small enough values
+    def withdraw_banknotes(self, amount: Decimal) -> list[Banknote]:
+        if amount < 0:
+            raise NegativeMoneyAmountException("Unable to withdraw negative amount of cash.")
+        if amount == 0:
+            return list[Banknote]
+        cash_available = self.get_cash_available()
+        if cash_available < amount:
+            raise NotEnoughMoneyException("There is not enough money in storage to withdraw", amount)
+
+        sorted_Banknotes = sorted(self.__banknotes, reverse=True)
+        banknotes_withdrawed = []
+        current_amount = Decimal()
+        while current_amount != amount and len(sorted_Banknotes) > 0:
+            current_banknote = sorted_Banknotes.pop(0)
+            if current_amount + current_banknote.value <= amount:
+                current_amount += current_banknote.value
+                banknotes_withdrawed.append(current_banknote)
+                self.__banknotes.remove(current_banknote)
+        
+        return banknotes_withdrawed
+
+    # Deposits banknotes to storage
+    def deposit_banknotes(self, banknotes: list[Banknote]) -> None:
+        self.__banknotes.extend(banknotes)
+
+# Exception raised when trying to pass negative money amount       
+class NegativeMoneyAmountException(Exception):
+    pass
+
+# Exception raised when storage doesn't have enough money to withdraw
+class NotEnoughMoneyException(Exception):
     pass
--- FirstLab/teller_machine_tests.py
+++ FirstLab/teller_machine_tests.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
 import unittest
 from teller_machine import Banknote
 from teller_machine import InvalidBanknoteValueException
-import teller_machine
+from teller_machine import BanknoteStorage
+from teller_machine import NegativeMoneyAmountException
 
 class TellerMachineTests(unittest.TestCase):
     def test_banknote_negative_value_set_raises_an_exception(self):
@@ -9,6 +11,116 @@ class TellerMachineTests(unittest.TestCase):
 
     def test_banknote_zero_value_set_raises_an_exception(self):
         self.assertRaises(InvalidBanknoteValueException, Banknote, 0)
+
+    def test_get_cash_available_empty_storage_returns_zero(self):
+        # Arrange
+        expected = Decimal()
+        storage = BanknoteStorage()
+
+        # Act
+        actual = storage.get_cash_available()
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    def test_get_cash_available_returns_calculated_cash(self):
+        # Arrange
+        banknotes = [Banknote(1), Banknote(2), Banknote(3), Banknote(4), Banknote(5)]
+        expected = Decimal(15)
+        storage = BanknoteStorage(banknotes)
+
+        # Act
+        actual = storage.get_cash_available()
+
+        # Assert
+        self.assertEqual(expected, actual)
+        
+    def test_withdraw_banknotes_negative_amount_raises_an_exception(self):
+        # Arrange
+        storage = BanknoteStorage()
+
+        # Act, Assert
+        self.assertRaises(NegativeMoneyAmountException, storage.withdraw_banknotes, -100)
+        
+    def test_withdraw_banknotes_amount_equals_zero_returns_empty_list(self):
+        # Arrange
+        storage = BanknoteStorage()
+        expected = list[Banknote]
+
+        # Act
+        actual = storage.withdraw_banknotes(0)
+         
+        # Assert
+        self.assertEqual(expected, actual)
+        
+    def test_withdraw_banknotes_returns_withdrawed_banknotes(self):
+        # Arrange
+        banknotes = [Banknote(1), Banknote(2), Banknote(3), Banknote(4), Banknote(5)]
+        storage = BanknoteStorage(banknotes)
+        amount_Expected = Decimal(8)
+        amount_Total = Decimal(15)
+        expected = [Banknote(5), Banknote(3)]
+
+        # Act
+        actual = storage.withdraw_banknotes(amount_Expected)
+         
+        # Assert
+        self.assertEqual(expected, actual)
+        self.assertEqual(amount_Total - amount_Expected, storage.get_cash_available())
+        
+    def test_withdraw_banknotes_doesnt_have_enough_banknotes_returns_closest(self):
+        # Arrange
+        banknotes = [Banknote(1), Banknote(2), Banknote(5), Banknote(10)]
+        storage = BanknoteStorage(banknotes)
+        amount = Decimal(9)
+        actual_amount_Expected = Decimal(8)
+        amount_Total = Decimal(18)
+        expected = [Banknote(5), Banknote(2), Banknote(1)]
+
+        # Act
+        actual = storage.withdraw_banknotes(amount)
+         
+        # Assert
+        self.assertEqual(expected, actual)
+        self.assertEqual(amount_Total - actual_amount_Expected, storage.get_cash_available())
+        
+    def test_withdraw_banknotes_doesnt_have_any_close_enough_banknotes_returns_empty_list(self):
+        # Arrange
+        banknotes = [Banknote(10), Banknote(20), Banknote(50), Banknote(100)]
+        storage = BanknoteStorage(banknotes)
+        amount_Expected = 180
+        amount = Decimal(9)
+
+        # Act
+        actual = storage.withdraw_banknotes(amount)
+         
+        # Assert
+        self.assertEqual(0, len(actual))
+        self.assertEqual(amount_Expected, storage.get_cash_available())
+        
+    def test_deposit_banknotes_adds_banknotes_to_empty_storage(self):
+        # Arrange
+        banknotes = [Banknote(1), Banknote(2), Banknote(3), Banknote(4), Banknote(5)]
+        amount_Expected = 15
+        storage = BanknoteStorage()
+
+        # Act
+        storage.deposit_banknotes(banknotes)
+         
+        # Assert
+        self.assertEqual(amount_Expected, storage.get_cash_available())
+        
+    def test_deposit_banknotes_adds_banknotes_to_empty_storage(self):
+        # Arrange
+        banknotes = [Banknote(1), Banknote(2), Banknote(3), Banknote(4), Banknote(5)]
+        amount_Expected = 15
+        storage = BanknoteStorage(banknotes)
+
+        # Act
+        storage.deposit_banknotes(banknotes)
+         
+        # Assert
+        self.assertEqual(amount_Expected * 2, storage.get_cash_available())
 
 if __name__ == "__main__":
     unittest.main()
